08c9943d2966d4522a3799d3a840a5d35b1e8aaf
Guillemet Samuel <samu.guillemet0@gmail.com>
2022-02-20 21:56:27 +0100

    Ajout de la fonction SetConstant sur Mario.cs

--- Assets/Scripts/Mario.cs
+++ Assets/Scripts/Mario.cs
@@ -12,6 +12,23 @@ public class Mario : MonoBehaviour
     }
     private input _currentInput;
 
+    public enum VelocityX
+    {
+        lowSpeed,
+        marche,
+        course
+    }
+    private VelocityX _currentVelocityX;                     //Velocité du joueur
+    private VelocityX _jumpVelocityX;                        //Vélocité au départ du saut
+
+    enum BodyDirection
+    {
+        gauche = -1,
+        arret = 0,
+        droite = 1,
+    }
+    private BodyDirection _currentBodyDirection;        //Direction voulue par le joueur
+
     //Composants associés au joueur
     private Rigidbody2D _rb;
     private Collider2D _col;
@@ -21,13 +38,12 @@ public class Mario : MonoBehaviour
     //Variable pour son déplacement
     private float _speedY;          //Vitesse verticale
     private float _absSpeedX;       //Vitesse absolue horizontale
-    private float _jumpVelocityX;   //Vitesse au départ du saut
 
     private bool _jump;
     private bool _crouch;
     private bool _grounded;
 
-    private int _direction;
+    private int _inputDirection;         //Direction selon l'input du joueur
     private float gravity;          //Current Gravity
     private float acceleration;     //Current acceleration
 
@@ -61,18 +77,21 @@ public class Mario : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        _speedY = _rb.velocity.y;
-        _absSpeedX = Mathf.Abs(_rb.velocity.x);
         _grounded = Physics2D.OverlapPoint(m_GroundCheck1.position, LayerMask.GetMask("Default")) || Physics2D.OverlapPoint(m_GroundCheck2.position, LayerMask.GetMask("Default"));
+
+        SetConstant();
         MoveMario();
 
-        _rb.velocity = new Vector2(5f * _direction, _rb.velocity.y); //TODO Fix the accelration value
+        _rb.velocity = new Vector2(5f * _inputDirection, _rb.velocity.y); //TODO Fix the accelration value
 
         _an.SetFloat("Speed", Mathf.Abs(_absSpeedX));
         _an.SetBool("Crouch", _crouch);
         _an.SetBool("Jumping", _jump);
     }
 
+    /// <summary>
+    /// The function that handle the deplacement of Mario
+    /// </summary>
     private void MoveMario()
     {
         _jump = Input.GetKey(KeyCode.UpArrow) && _speedY >= 0;
@@ -81,15 +100,15 @@ public class Mario : MonoBehaviour
         {
             _currentInput = input.marche;
             if (Input.GetKey(KeyCode.LeftArrow)) //Direction
-                _direction = -1;
+                _inputDirection = -1;
             else
-                _direction = 1;
-            transform.localScale = new Vector2(Mathf.Sign(_direction), transform.localScale.y);
+                _inputDirection = 1;
+            transform.localScale = new Vector2(Mathf.Sign(_inputDirection), transform.localScale.y);
         }
         else
         {
             _currentInput = input.arret;
-            _direction = 0;
+            _inputDirection = 0;
         }
 
         if (Input.GetKey(KeyCode.Space) && _currentInput == input.marche) //Run
@@ -97,7 +116,7 @@ public class Mario : MonoBehaviour
 
         if (Input.GetKey(KeyCode.UpArrow) && _grounded) //Jump
         {
-            _jumpVelocityX = _rb.velocity.x;
+            _jumpVelocityX = _currentVelocityX;
             _rb.velocity = new Vector2(_rb.velocity.x, 15); //TODO Fix the speed of jumping
         }
 
@@ -106,4 +125,28 @@ public class Mario : MonoBehaviour
         else
             _crouch = false;
     }
+
+    /// <summary>
+    /// The function that handle the different constant
+    /// </summary>
+    private void SetConstant()
+    {
+        if (_rb.velocity.x > 0)
+            _currentBodyDirection = BodyDirection.droite;
+        if (_rb.velocity.x == 0)
+            _currentBodyDirection = BodyDirection.arret;
+        if (_rb.velocity.x < 0)
+            _currentBodyDirection = BodyDirection.gauche;
+
+        if (_absSpeedX < lowSpeed)
+            _currentVelocityX = VelocityX.lowSpeed;
+        if (_absSpeedX > lowSpeed && _absSpeedX < marcheSpeed)
+            _currentVelocityX = VelocityX.marche;
+        if (_absSpeedX > marcheSpeed)
+            _currentVelocityX = VelocityX.course;
+
+        _speedY = _rb.velocity.y;
+        _absSpeedX = Mathf.Abs(_rb.velocity.x);
+    }
 }
+
