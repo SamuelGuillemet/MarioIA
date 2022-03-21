9bbe2c865c1bb350f12dd38b1f6b99a7e80e7525
Thibaut Sauv <sauvthibaut@gmail.com>
2022-03-22 00:47:40 +0100

    Minor fixes to the animation function
    Could not previously make the "Bricks" block be replaced by none.
    Now fixed.

--- Assets/Scripts/brick.cs
+++ Assets/Scripts/brick.cs
@@ -20,51 +20,46 @@ public class brick : MonoBehaviour
     {
         if (col.gameObject.name == "BabyMario")
         {
-            if (NextPrefab)
+            //if mario hits from under and animation not already started
+            if (_posInit[0] - 0.5 <= col.GetContact(0).point[0] && col.GetContact(0).point[0] <= _posInit[0] + 0.5 && col.GetContact(0).point[1] <= _posInit[1] && !_isAnimated)
             {
-                //if mario hits from under and animation not already started
-                if (_posInit[0] - 0.5 <= col.GetContact(0).point[0] && col.GetContact(0).point[0] <= _posInit[0] + 0.5 && col.GetContact(0).point[1] <= _posInit[1] && !_isAnimated)
+                foreach (GameObject enemy in _enemies.ToArray())
                 {
-
-                    foreach (GameObject enemy in _enemies.ToArray())
-                    {
-                        FlipAndKill(enemy);
-                    }
-
-                    StartCoroutine("BrickHit");
-
-
+                    FlipAndKill(enemy);
                 }
 
-
-
-
+                StartCoroutine("BrickHit");
             }
-
         }
     }
+
+
     private IEnumerator BrickHit()
     {
         _isAnimated = true;
+
         for (int i = 0; i < 16; i++)
         {
             this.transform.position = new Vector2(_posInit[0], _posInit[1] + 0.03125F * i);
             yield return null;
         }
+
         for (int i = 16; i > 0; i--)
         {
             this.transform.position = new Vector2(_posInit[0], _posInit[1] + 0.03125F * i);
             yield return null;
         }
 
-        Instantiate(NextPrefab, _posInit, Quaternion.identity);
-        Destroy(gameObject);
+        if (NextPrefab)
+        {
+            Instantiate(NextPrefab, _posInit, Quaternion.identity);
+        }
 
+        Destroy(gameObject);
     }
 
     void FlipAndKill(GameObject enemy)
     {
 
     }
-
 }
