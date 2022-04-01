6642d97a4197a22ebf0eca537bfe79479c28ccb6
Guillemet Samuel <samu.guillemet0@gmail.com>
2022-04-01 14:57:27 +0200

    Create the animation

--- Assets/Scripts/HammerBros.cs
+++ Assets/Scripts/HammerBros.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class HammerBros : MonoBehaviour
 {
+    public int _countJump = 0;
+    public bool ShouldJump = false;
+
+    public bool ThrowHammer = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
